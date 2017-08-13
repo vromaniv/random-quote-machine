07d1586fd7d5c4f0279d53e3aaf12cbcbc6c66a9
Vitaliy Romaniv <vit.romaniv@gmail.com>
2017-08-13 19:58:24 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(getQuote);
+
+$("#quote_btn").click(getQuote);
+
+function getQuote() {
+    $.getJSON("http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=?", function (json) {
+        let quote = json.quoteText;
+        let author = json.quoteAuthor;
+        console.log(JSON.stringify(json));
+        $('#quote_text').fadeOut(400, function () {
+            $(this).html(quote).fadeIn(400);
+        });
+        $('#author').fadeOut(400, function () {
+            if (author == "") {
+                $(this).html('Unknown').fadeIn(400);
+            } else {
+                $(this).html(author).fadeIn(400);
+            }
+        });
+        $('#tweet_btn').attr('href', 'https://twitter.com/intent/tweet?text="' + quote + '" - ' + author);
+    });
+}
